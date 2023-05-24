9155a2a86b7f1031f86a048b5d2349704de97224
pedrolucas.leal@gmail.com <pedrolucas.leal@gmail.com>
2023-05-24 09:04:53 -0300

    Create php1.php

---
+++ php1.php
@@ -0,0 +1,14 @@
+$anos = 20;
+$mes = 7;
+$dist = 15;
+
+echo "Amanda tem $anos anos!";
+echo '<br />';
+echo 'Amanda tem $anos anos!';
+echo '<br />;
+echo "No mês", $mes, " Fabiana completara {$anos}anos!";
+echo '<br />';
+echo "no mês", $mes, "Teremos ", $anos, " anos de casa.";
+echo '<br >/;
+echo "Percorreu {$dist}km<br />";
+echo "Vinicius percorreus ".$dist.'Km';
